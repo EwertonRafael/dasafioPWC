e727d14d4fa8ac43dedf65d6655d08bab7ba10ef
Ewerton Rafael <ton_rafael@hotmail.com>
2023-07-07 17:27:02 -0300

    quarto exercício

---
+++ questao4/src/application/Program.java
@@ -0,0 +1,33 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		/*
+		 * coloque em maiúscula a primeira letra de cada frase da string a) input:
+		 * "hello, how are you? i'm fine, thank you." b) output:
+		 * "Hello, how are you? I'm fine, thank you."
+		 */
+
+		String phrase = "hello, how are you? i'm fine, thank you.";
+		System.out.println(capitalize(phrase));
+	}
+
+	private static String capitalize(String phrase) {
+		StringBuilder sb = new StringBuilder();
+		boolean uppercaseLetter = true;
+
+		for (char c : phrase.toCharArray()) {
+			if (uppercaseLetter && Character.isLetter(c)) {
+				sb.append(Character.toUpperCase(c));
+				uppercaseLetter = false;
+			} else {
+				sb.append(c);
+			}
+			if (c == '.' || c == '?' || c == '!') {
+				uppercaseLetter = true;
+			}
+		}
+		return sb.toString();
+	}
+}
